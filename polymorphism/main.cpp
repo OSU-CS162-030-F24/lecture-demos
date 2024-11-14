a731c94becdc016bddcb1c45c09a705fe766fb44
Alexander Guyer <guyera@oregonstate.edu>
2024-11-13 17:02:00 -0800

    11/13 lecture

--- polymorphism/main.cpp
+++ polymorphism/main.cpp
@@ -4,13 +4,12 @@
 #include "vampire.hpp"
 #include "zombie.hpp"
 
-void do_turn(vampire* vampires, int n_vampires, zombie* zombies, int n_zombies, player& p) {
-	for (int i = 0; i < n_vampires; i++) {
-		vampires[i].do_turn(p);
-	}
+// Polymorphism: A variable that can take on one of many types.
+// 	"Many forms"
 
-	for (int i = 0; i < n_zombies; i++) {
-		zombies[i].do_turn(p);
+void do_turn(monster** monsters, int n_monsters, player& p) {
+	for (int i = 0; i < n_monsters; i++) {
+		monsters[i]->do_turn(p);
 	}
 }
 
@@ -21,15 +20,26 @@ int main() {
 	// variable lengths, so we use dynamic arrays
 	int n_vampires = 10;
 	int n_zombies = 5;
-	vampire* vampires = new vampire[n_vampires];
-	zombie* zombies = new zombie[n_zombies];
+	int n_monsters = n_vampires + n_zombies;
+	monster** monsters = new monster*[n_monsters];
+
+	for (int i = 0; i < n_vampires; i++) {
+		vampire* v = new vampire;
+		monsters[i] = v; // This is upcasting!
+	}
+	for (int i = n_vampires; i < n_monsters; i++) {
+		zombie* z = new zombie;
+		monsters[i] = z; // This is upcasting!
+	}
 
 	while (p.alive()) {
-		do_turn(vampires, n_vampires, zombies, n_zombies, p);
+		do_turn(monsters, n_monsters, p);
 		p.print();
 		std::cout << std::endl;
 	}
 
-	delete [] vampires;
-	delete [] zombies;
+	for (int i = 0; i < n_monsters; i++) {
+		// delete monsters[i];
+	}
+	delete [] monsters;
 }

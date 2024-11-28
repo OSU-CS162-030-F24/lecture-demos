3b7fec3dd0605a20ffff011f3220182876e80e50
Alexander Guyer <guyera@oregonstate.edu>
2024-11-27 17:00:48 -0800

    11/27 lecture

--- recursion/main.cpp
+++ recursion/main.cpp
@@ -25,8 +25,67 @@ int fib(int n) {
 
 // Every function call has its own "stack frame"
 
+// Write a function called print_x(n) that prints an X
+// to the terminal made up of a bunch of *'s of height n
+
+/*
+ * print_x(7) should print the following:
+ *
+ *    *   *
+ *     * *
+ *      *
+ *     * *
+ *    *   *
+ *
+ */
+
+void print_x(int height, int padding) {
+	if (height == 1) {
+		// Print an x of height 1
+		for (int i = 0; i < padding; i++) {
+			std::cout << ' ';
+		}
+		std::cout << '*';
+		std::cout << std::endl;
+
+		return;
+	}
+
+	// Print the padding before the top row
+	for (int i = 0; i < padding; i++) {
+		std::cout << ' ';
+	}
+	// Print the first asterisk in the top row
+	std::cout << '*';
+	// Print all of the spaces in the top row
+	for (int i = 0; i < height - 2; i++) {
+		std::cout << ' ';
+	}
+	// Print the second asterisk in the top row
+	std::cout << '*';
+	std::cout << std::endl;
+
+	print_x(height - 2, padding + 1); // Recursive call
+
+	// Print the padding before the bottom row
+	for (int i = 0; i < padding; i++) {
+		std::cout << ' ';
+	}
+	// Print the first asterisk in the bottom row
+	std::cout << '*';
+	// Print all of the spaces in the bottom row
+	for (int i = 0; i < height - 2; i++) {
+		std::cout << ' ';
+	}
+	// Print the second asterisk in the bottom row
+	std::cout << '*';
+	std::cout << std::endl;
+}
+
 int main() {
 	print_hello_n_times(10);
 
-	std::cout << fib(100) << std::endl;
+	// std::cout << fib(1) << std::endl;
+
+	print_x(5, 0);
 }
